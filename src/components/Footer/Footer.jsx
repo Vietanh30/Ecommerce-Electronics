08d7d05c2c1c5d5256b648a27a0ad07755d0463a
VietAnh30 <vietanhdang487@gmail.com>
2024-11-22 13:34:45 +0700

    fix: footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
           <div>
             <p className="font-bold mb-2">Về công ty</p>
             <ul className="list-none">
-              <li className='mt-2'><Link rel="nofollow" className="hover:underline">Giới thiệu công ty (MWG.vn)</Link></li>
+              <li className='mt-2'><Link rel="nofollow" className="hover:underline">Giới thiệu công ty (Nhóm 10)</Link></li>
               <li className='mt-2'><Link  rel="nofollow" className="hover:underline">Tuyển dụng</Link></li>
               <li className='mt-2'><Link  rel="nofollow" className="hover:underline">Gửi góp ý, khiếu nại</Link></li>
               <li className='mt-2'><Link  rel="nofollow" className="hover:underline">Tìm siêu thị (2965 shop)</Link></li>
@@ -48,40 +48,11 @@ const Footer = () => {
             <p className="font-bold mb-2">Website cùng tập đoàn</p>
             <ul className="flex flex-wrap gap-2">
               <li>
-                <Link href="https://www.thegioididong.com/" target="_blank" rel="nofollow" aria-label="logo thegioididong" className="hover:underline">
-                  <i className="iconlogo-thegioididong"></i>
+                <Link to="https://donga.edu.vn/" target="_blank" rel="nofollow" aria-label="logo thegioididong" className="hover:underline text-blue-600 font-semibold">
+                  Nhóm 10 - DongA University
                 </Link>
               </li>
-              <li>
-                <Link href="https://www.topzone.vn/" target="_blank" rel="nofollow" title="Chuỗi cửa hàng cao cấp Apple" aria-label="logo topzone" className="hover:underline">
-                  <i className="iconlogo-topzone"></i>
-                </Link>
-              </li>
-              <li>
-                <Link href="https://www.bachhoaxanh.com/" target="_blank" rel="nofollow" aria-label="logo bachhoaxanh" className="hover:underline">
-                  <i className="iconlogo-bachhoaxanh"></i>
-                </Link>
-              </li>
-              <li>
-                <Link href="https://www.nhathuocankhang.com/" target="_blank" rel="nofollow" title="Chuỗi nhà thuốc chuẩn GPP" aria-label="logo ankhang" className="hover:underline">
-                  <i className="iconlogo-ankhang"></i>
-                </Link>
-              </li>
-              <li>
-                <Link href="https://www.avakids.com/" target="_blank" rel="nofollow" title="Chuỗi cửa hàng mẹ và bé" aria-label="logo avakids" className="hover:underline">
-                  <i className="iconlogo-kids"></i>
-                </Link>
-              </li>
-              <li>
-                <Link href="https://vieclam.thegioididong.com/" target="_blank" rel="nofollow" title="Trang tuyển dụng của tập đoàn Thế Giới Di Động" aria-label="logo vieclam" className="hover:underline">
-                  <i className="iconlogo-vieclam"></i>
-                </Link>
-              </li>
-              <li>
-                <Link href="https://www.erablue.id/" target="_blank" rel="nofollow" title="Toko Erablue Elektronik" aria-label="logo erablue" className="hover:underline">
-                  <i className="iconlogo-erablue"></i>
-                </Link>
-              </li>
+              
             </ul>
           </div>
         </div>
@@ -90,9 +61,7 @@ const Footer = () => {
         <div className="container mx-auto px-4 lg:px-20">
             <section className="text-center">
             <p className="text-sm">
-                © 2018. Công ty cổ phần Thế Giới Di Động. GPDKKD: 0303217354 do sở KH & ĐT TP.HCM cấp ngày 02/01/2007. GPMXH: 21/GP-BTTTT do Bộ Thông Tin và Truyền Thông cấp ngày 11/01/2021.<br />
-                Địa chỉ: 128 Trần Quang Khải, P.Tân Định, Q.1, TP.Hồ Chí Minh. Địa chỉ liên hệ và gửi chứng từ: Lô T2-1.2, Đường D1, Đ. D1, P.Tân Phú, TP.Thủ Đức, TP.Hồ Chí Minh. Điện thoại: 028 38125960. Email: <Link href="mailto:cskh@thegioididong.com" className="text-blue-500">cskh@thegioididong.com</Link>. Chịu trách nhiệm nội dung: Huỳnh Văn Tốt. Email: <Link href="mailto:hotrotmdt@thegioididong.com" className="text-blue-500">hotrotmdt@thegioididong.com</Link>.
-                <Link href="/thoa-thuan-su-dung-trang-mxh" className="text-blue-500 hover:underline">Xem chính sách sử dụng</Link>
+            Copyright © 2024 - Dong A University
             </p>
             </section>
         </div>
